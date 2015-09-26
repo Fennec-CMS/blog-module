757cdb4e70c97eec5f12a80c04d3600b219b0a21
David Lima <dave@blogdodave.com.br>
2015-09-26 13:44:16 -0300

    Including Admin/Menu.php file

---
+++ Admin/Menu.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    'Blog' => array(
+        'route' => 'admin-blog-list',
+        'icon' => 'fa fa-pencil',
+        'subitems' => array(
+            'Write new post' => 'admin-blog-write',
+            'List' => 'admin-blog-list'
+        )
+    )
+);
